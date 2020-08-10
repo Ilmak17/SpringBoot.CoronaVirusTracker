16fe4ebe365da7942be8ca5fa3ffb54deb71e08f
Ilyas Makirov <Ilmak1704@gmail.com>
2020-08-10 20:07:09 +0600

    first commit

---
+++ src/main/java/io/javabrains/coronavirustracker/services/models/LocationStats.java
@@ -0,0 +1,50 @@
+package io.javabrains.coronavirustracker.services.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
+
+    public String getState(String s) {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String county) {
+        this.country = county;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", county='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
